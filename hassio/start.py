krkeegan/insteon-mqtt

commit 9e9f7061c47b2174bcd0e9e8cbee21aa49d47eaa

Add Version Command

insteon_mqtt/Modem.py
@@ -7,6 +7,7 @@ import json
 import os
 import sys
 import functools
+import insteon_mqtt
 from .Address import Address
 from .CommandSeq import CommandSeq
 from . import config
@@ -89,7 +90,8 @@ class Modem:
             'sync_all' : self.sync_all,
             'sync' : self.sync,
             'import_scenes': self.import_scenes,
-            'import_scenes_all': self.import_scenes_all
+            'import_scenes_all': self.import_scenes_all,
+            'version': self.version
             }
 
         # Add a generic read handler for any broadcast messages initiated by
@@ -244,6 +246,16 @@ class Modem:
         msg = Msg.OutModemInfo()
         msg_handler = handler.ModemInfo(self, on_done)
         self.send(msg, msg_handler)
+
+    #-----------------------------------------------------------------------
+    def version(self, on_done=None):
+        """ Returns the version of insteon_mqtt
+
+        Used by the MQTT command:
+          Default Topic: 'insteon/command/modem'
+          Payload: '{"cmd": "version"}'
+        """
+        on_done(True, insteon_mqtt.__version__, None)
 
     #-----------------------------------------------------------------------
     def refresh(self, force=False, on_done=None):

insteon_mqtt/cmd_line/main.py
@@ -5,6 +5,7 @@
 #===========================================================================
 import argparse
 import sys
+import insteon_mqtt
 from .. import config
 from . import device
 from . import modem
@@ -17,6 +18,8 @@ def parse_args(args):
     # pylint: disable=too-many-statements
     p = argparse.ArgumentParser(prog="insteon-mqtt",
                                 description="Insteon<->MQTT tool")
+    p.add_argument('-v', '--version', action='version', version='%(prog)s ' +
+                   insteon_mqtt.__version__)
     p.add_argument("config", metavar="config.yaml", help="Configuration "
                    "file to use.")
     sub = p.add_subparsers(help="Command help")

insteon_mqtt/mqtt/Mqtt.py
@@ -6,6 +6,7 @@
 import functools
 import json
 import logging
+import insteon_mqtt
 from .. import log
 from . import config
 from .MsgTemplate import MsgTemplate

commit 7f7e2ca208f908497e5ef2a0976e64cdd8d8f19a

Fix Cyclic Import Error
This is Option #1

This fixes a pylint error that was introduced when I added the
version feature.  It caused a Cyclic Import error because I was
re-importing the base insteon_mqtt module over again.  While
things worked, I agree with pylint that this is a dangerous
design.

I am not sure what the proper solution is.  I would like to access
the __version__ variable in the base module from other classes
that do not inherit from this base module.

This solution uses __main__ which looks reasonable elegant enough
but requires ignoring a linter error that __main__ is undefined,
because it is, until runtime.

Not super thrilled by this as someone calling the start method
not from the command line could get an error.

Error introduced in: 9e9f7061c47b2174bcd0e9e8cbee21aa49d47eaa

hassio/start.py
@@ -5,6 +5,8 @@ import threading
 import insteon_mqtt
 import webcli
 
+__version__ = insteon_mqtt.__version__
+
 threading.Thread(target=webcli.app.start_webcli, daemon=True).start()
 status = insteon_mqtt.cmd_line.main()
 sys.exit(status)

insteon_mqtt/Modem.py
@@ -7,7 +7,7 @@ import json
 import os
 import sys
 import functools
-import insteon_mqtt
+import __main__
 from .Address import Address
 from .CommandSeq import CommandSeq
 from . import config
@@ -256,7 +256,7 @@ class Modem:
           Default Topic: 'insteon/command/modem'
           Payload: '{"cmd": "version"}'
         """
-        on_done(True, insteon_mqtt.__version__, None)
+        on_done(True, __main__.__version__, None)  # pylint:disable=I1101
 
     #-----------------------------------------------------------------------
     def refresh(self, force=False, on_done=None):

insteon_mqtt/cmd_line/main.py
@@ -5,7 +5,7 @@
 #===========================================================================
 import argparse
 import sys
-import insteon_mqtt
+import __main__
 from .. import config
 from . import device
 from . import modem
@@ -19,7 +19,7 @@ def parse_args(args):
     p = argparse.ArgumentParser(prog="insteon-mqtt",
                                 description="Insteon<->MQTT tool")
     p.add_argument('-v', '--version', action='version', version='%(prog)s ' +
-                   insteon_mqtt.__version__)
+                   __main__.__version__)  # pylint:disable=I1101
     p.add_argument("config", metavar="config.yaml", help="Configuration "
                    "file to use.")
     sub = p.add_subparsers(help="Command help")

scripts/insteon-mqtt
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
-
 import insteon_mqtt
+
+__version__ = insteon_mqtt.__version__
 status = insteon_mqtt.cmd_line.main()
 sys.exit(status)

tests/test_Integration.py
@@ -27,6 +27,9 @@ import pytest
 import json
 from insteon_mqtt import log
 import insteon_mqtt as IM
+import __main__
+
+__main__.__version__ = IM.__version__
 
 
 def test_set_on_functions(stack):